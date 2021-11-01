e27102081f0b18b716376904544c1aed577e0961
Thiago Pereira <thiago_salper@hotmail.com>
2021-11-01 14:58:31 -0300

    chore: size constants

---
+++ src/constants/index.ts
@@ -0,0 +1,1 @@
+export { default as ScreenSize } from './sizes';
---
+++ src/constants/sizes.ts
@@ -0,0 +1,9 @@
+import { Dimensions } from "react-native";
+
+const SCREEN_WIDTH = Dimensions.get('screen').width;
+const SCREEN_HEIGHT = Dimensions.get('screen').height;
+
+export default {
+  WIDTH: SCREEN_WIDTH,
+  HEIGHT: SCREEN_HEIGHT,
+}
